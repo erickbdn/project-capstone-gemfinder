23b3ca357f1ea607a5433a37aded951af7d47326
FawwazKats <fawwazkautsar1907912@upi.edu>
2022-06-10 04:11:36 +0700

    Membuat tampilan pada elemen hero

--- src/scripts/views/pages/venue-list.js
+++ src/scripts/views/pages/venue-list.js
@@ -4,6 +4,15 @@ import DATA from '../../data/venue.json';
 const VenueList = {
   async render() {
     return `
+      <div class="hero">
+        <div class="container">
+          <h1 class="hero__title">Jelajahi Destinasi Indah Tersembunyi</h1>
+          <p class="hero__tagline">Bangun Suasana Hatimu dengan Berwisata yang Menyenangkan!</p>
+          <a href="#content" class="btn">Find Now!</a>
+        </div>
+      </div>
+      <h1 class="empty"></h1>
+
       <h2>Venue All Star</h2>
 
       <div class="content">
