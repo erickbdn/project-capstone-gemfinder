88b0ee96fd6b25da0e57d50db392b76044847334
FawwazKats <fawwazkautsar1907912@upi.edu>
2022-06-12 04:54:02 +0700

    Membuat kolom testimonial

--- src/scripts/views/pages/venue-list.js
+++ src/scripts/views/pages/venue-list.js
@@ -18,6 +18,34 @@ const VenueList = {
       <div class="content">
         <div id="venue-list" class="venue-list"></div>
       </div>
+
+      <div class="testimonial">
+        <div class="small-container">
+          <div class="row">
+            <div class="col-3">
+              <i class="fa fa-quote-left"></i>
+              <p class="text">Amazing holiday with GemFinder.</p>
+              <p>&bigstar; &bigstar; &bigstar; &bigstar; &star;</p>
+              <img src="./images/user-1.jpg">
+              <h3>Emily Victoria</h3>
+            </div>
+            <div class="col-3">
+              <i class="fa fa-quote-left"></i>
+              <p class="text">Amazing holiday with GemFinder.</p>
+              <p>&bigstar; &bigstar; &bigstar; &bigstar; &bigstar;</p>
+              <img src="./images/user-2.jpg">
+              <h3>Asher Zachary</h3>
+            </div>
+            <div class="col-3">
+              <i class="fa fa-quote-left"></i>
+              <p class="text">Amazing holiday with GemFinder.</p>
+              <p>&bigstar; &bigstar; &bigstar; &bigstar; &star;</p>
+              <img src="./images/user-3.jpg">
+              <h3>Grace Madison</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     `;
   },
 
