c6ef974e9be9effa9807c54ca33867a6dc2ef4db
Tederick Budiono <100257124+erickbdn@users.noreply.github.com>
2022-06-16 20:01:04 +0700

    Add files via upload
    menambahkan endpoint dan config (untuk hosting json menggunakan layanan npoint, untuk hosting image menggunakan github pages)

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  BASE_URL: 'https://api.npoint.io/5a17b6c3d44bfad564f1/',
+  BASE_IMAGE_URL: 'https://erickbdn.github.io/image-hosting/',
+  CACHE_NAME: 'GemFinder_v1',
+  DATABASE_NAME: 'gemfinder-database',
+  DATABASE_VERSION: '1',
+  OBJECT_STORE_NAME: 'locations',
+};
+
+export default CONFIG;
