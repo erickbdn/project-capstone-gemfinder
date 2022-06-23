6219229cc7d1c1c579891c8fa13e44f412978438
Tederick Budiono <100257124+erickbdn@users.noreply.github.com>
2022-06-23 18:29:21 +0700

    Add files via upload

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
   BASE_URL: 'https://api.npoint.io/5a17b6c3d44bfad564f1/',
   BASE_IMAGE_URL: 'https://erickbdn.github.io/image-hosting/',
-  CACHE_NAME: 'GemFinder_v1',
-  DATABASE_NAME: 'gemfinder-database',
-  DATABASE_VERSION: '1',
-  OBJECT_STORE_NAME: 'locations',
+  CACHE_NAME: 'GemFinder-V1',
+  DATABASE_NAME: 'venue-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'venues',
 };
 
 export default CONFIG;
