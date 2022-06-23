6e65032ae6851017181f5c1e93174e675ca8795a
Tederick Budiono <100257124+erickbdn@users.noreply.github.com>
2022-06-23 18:32:54 +0700

    Add files via upload

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,46 +1,65 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/prefer-default-export */
 import CONFIG from '../../globals/config';
 
-const createLocationDetailTemplate = (details) => `
+const createVenueItemTemplate = (location) => `
+<div class="venue-item">
+    <div class="venue-item__header">
+        <img tabindex="0" 
+            class="venue-item__header__poster" 
+            src="${CONFIG.BASE_IMAGE_URL + location.image}" 
+            alt="${location.name}." 
+            height="170px"
+       />
+       <div class="venue-item__header__rating">
+            <p>⭐️<span class="venue-item__header__rating__score">${location.rating || '-'}</span></p>
+        </div>
+    </div>
+    <div class="venue-item__content">
+      <h1 tabindex="-1" class="venue-item__title">
+        <a tabindex="0" class="venue-item__content__link" href="${`/#/detail/${location.id}`}">
+          <span class=  venue-item__content__title>
+            ${location.name}
+          </span>
+        </a>
+      </h1>
+      <p tabindex="0" class="venue-item__description">${location.description}</p>
+    </div>
+  </div>
+`;
 
-<div class="location_info">
-    <div class="location_info_header">
-        <img class="location_info_image" src="${CONFIG.BASE_IMAGE_URL + details.image}" alt="${details.name}"/>
-    </div>
-    <div class="location_info_content">
-        <h3>Nama Lokasi</h3>
-        <p class="location_name">${details.name}</p>
-        <h3>Kabupaten / Kota</h3>
-        <p class="location_kabupatenkota">${details.kabupatenkota}</p>
-        <h3>Provinsi</h3>
-        <p class="location_provinsi">${details.provinsi}</p>
-        <h3>Alamat</h3>
-        <p class="location_address">${details.address}</p>
-        <h3>Deskripsi</h3>
-        <p class="location_description">${details.description}</p>
-        <h3>Rating</h3>
-        <p class="location_rating">${details.rating}</p>
-    </div>
+const createVenueDetailTemplate = (detail) => `
+<h2 class="venue__title">${detail.name}</h2>
+  <img class="venue__poster" src="${CONFIG.BASE_IMAGE_URL + detail.image}" alt="${detail.name}" />
+  <div class="venue__info">
+  <h3>Information</h3>
+    <h4>City</h4>
+    <p>${detail.kabupatenkota}</p>
+    <h4>Rating</h4>
+    <p>${detail.rating}</p>
+    <h4>Description</h4>
+    <p>${detail.description}</p>
+  </div>
+  <div class="venue__overview">
+    <h3>Overview</h3>
+    <p>${detail.rating}</p>
+  </div>
 </div>
 `;
 
-const createLocationItemTemplate = (location) => `
-
-<div class="location-item">
-    <div class="location-item-header">
-        <div class="location-item-header-rating">
-            <p>⭐️<span class="location-item-header-rating-score">${location.rating}</span></p>
-        </div>
-        <div class="location_item_header">
-            <img class="location_item_image" src="${CONFIG.BASE_IMAGE_URL + location.image}" alt="${location.name}"/>
-        </div>
-    </div>
-    <div class="location-item-content">
-        <h3><a href="${`/#/detail/${location.id}`}">${location.name}</a></h3>
-        <p>${location.description}</p>
-    </div>
-</div>    
+const createLikeButtonTemplate = () => `
+  <button aria-label="like this venue" id="likeButton" class="like">
+     <i class="fa fa-heart-o" aria-hidden="true"></i>
+  </button>
 `;
 
-export { createLocationDetailTemplate, createLocationItemTemplate };
+const createLikedButtonTemplate = () => `
+  <button aria-label="unlike this venue" id="likeButton" class="like">
+    <i class="fa fa-heart" aria-hidden="true"></i>
+  </button>
+`;
+
+export {
+  createVenueItemTemplate,
+  createVenueDetailTemplate,
+  createLikeButtonTemplate,
+  createLikedButtonTemplate,
+};
