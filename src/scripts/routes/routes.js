d51858f6e6ee5c850fd6b8361171b15912ac6ce3
Tederick Budiono <100257124+erickbdn@users.noreply.github.com>
2022-06-29 19:27:44 +0700

    Add files via upload
    Menambahkan route untuk halaman risk

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import VenueList from '../views/pages/venue-list';
 import VenueFullList from '../views/pages/venue-full-list';
 import Wishlist from '../views/pages/wishlist';
 import Detail from '../views/pages/detail';
+import Risk from '../views/pages/risk';
 
 const routes = {
   '/': VenueList,
   '/list': VenueFullList,
   '/wishlist': Wishlist,
   '/detail/:id': Detail,
+  '/risk': Risk,
 };
 
 export default routes;
