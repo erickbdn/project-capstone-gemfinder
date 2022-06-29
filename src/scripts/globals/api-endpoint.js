f02299cf1dedfc3f9dd0dd70b400cfae3878fc02
Tederick Budiono <100257124+erickbdn@users.noreply.github.com>
2022-06-29 19:26:59 +0700

    Add files via upload
    Menambahkan BASE_COVID_URL dan endpoint dari url covid

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -3,6 +3,7 @@ import CONFIG from './config';
 const API_ENDPOINT = {
   LOCATION: `${CONFIG.BASE_URL}locations`,
   DETAIL: (id) => `${CONFIG.BASE_URL}details/${id - 1}`,
+  COVID: `${CONFIG.BASE_COVID_URL}skor.json`,
 };
 
 export default API_ENDPOINT;
