0ff2eed00c2ddaf1ae74044f736818079793b845
Tederick Budiono <100257124+erickbdn@users.noreply.github.com>
2022-07-01 23:06:12 +0700

    Add files via upload
    Menambahkan route untuk halaman about us

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -3,12 +3,14 @@ import VenueFullList from '../views/pages/venue-full-list';
 import Wishlist from '../views/pages/wishlist';
 import Detail from '../views/pages/detail';
 import Risk from '../views/pages/risk';
+import AboutUs from '../views/pages/about-us';
 
 const routes = {
   '/': VenueList,
   '/list': VenueFullList,
   '/wishlist': Wishlist,
   '/detail/:id': Detail,
+  '/about': AboutUs,
   '/risk': Risk,
 };
 
