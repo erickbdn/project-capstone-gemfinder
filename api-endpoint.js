94b6096dfa7279a57107182a559194ece54b0f06
Zhafran Afif <91825801+zhafranafif@users.noreply.github.com>
2022-07-02 20:27:12 +0700

    Delete api-endpoint.js

--- api-endpoint.js
+++
@@ -1,8 +0,0 @@
-import CONFIG from './config';
-
-const API_ENDPOINT = {
-  LOCATION: `${CONFIG.BASE_URL}locations`,
-  DETAIL: (id) => `${CONFIG.BASE_URL}details/${id - 1}`,
-};
-
-export default API_ENDPOINT;
